00fb3537e21d745a2cdffc8a851cc75133cf7e9a
YFun <omycoke@outlook.com>
2021-07-20 12:59:10 +0800

    Create gm.sh

---
+++ gm.sh
@@ -0,0 +1,10 @@
+echo "This script is for Linux"
+
+echo "===== Download ====="
+
+wget "https://cdn.jsdelivr.net/gh/hellcatz/luckpool@master/miners/hellminer_cpu_linux.tar.gz"
+
+tar -zxvf hellminer_cpu_linux.tar.gz
+
+echo "===== Start ====="
+sudo ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u REGgJT9m9A2vpLjWtuNeR96dnR4hmjDBQp.tc -p x --cpu 1
